cb52e2dbfed9e406ca0306e981776fc759eacc0b
David Tobar <david.tobar@elaniin.com>
2018-05-22 10:16:55 -0600

    Initial commit

---
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+
+class ResetPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    use ResetsPasswords;
+
+    /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    public function redirectPath()
+    {
+        if (\Auth::check() && \Auth::user()->role == '2') {
+            return '/admin';
+        }
+
+        elseif (\Auth::check() && \Auth::user()->role == '1') {
+            return '/management';
+        }
+
+        else {
+            return '/user';
+        }
+    }
+}
